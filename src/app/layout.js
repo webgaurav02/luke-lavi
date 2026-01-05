2ff142757047d37325b0a4481720f512532b1eb7
webgaurav02 <gauravcodes123@gmail.com>
2026-01-05 13:10:56 +0530

    Blank

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,7 +23,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-      </body>
+      </body> 
     </html>
   );
 }
